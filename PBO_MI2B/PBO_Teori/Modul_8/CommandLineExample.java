25991fe8076c63d20a83d67b8a84e08917ec18bf
satriacoding <118911569+satriacoding@users.noreply.github.com>
2022-12-08 12:09:21 +0700

    Create CommandLineExample.java

---
+++ PBO_MI2B/PBO_Teori/Modul_8/CommandLineExample.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_Modul8;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CommandLineExample
+{
+ public static void main( String[] args ){
+
+ System.out.println("Number of arguments=" +
+ args.length);
+ System.out.println(""+ args[0]);
+ System.out.println(""+ args[1]);
+ System.out.println(""+ args[2]);
+ System.out.println(""+ args[3]);
+ System.out.println(""+ args[4]);
+ }
+} 
