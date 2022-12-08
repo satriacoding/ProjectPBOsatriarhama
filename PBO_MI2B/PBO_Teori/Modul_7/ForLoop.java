38468d1c20cba44273fcf1386fef23c26dca4c62
satriacoding <118911569+satriacoding@users.noreply.github.com>
2022-12-08 12:08:52 +0700

    Create ForLoop.java

---
+++ PBO_MI2B/PBO_Teori/Modul_7/ForLoop.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_Modul7;
+
+/**
+ *
+ * 
+ */
+public class ForLoop {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+        for(int i=0;i<hari.length;i++)
+            {
+                System.out.println(hari[i]);
+                ;
+            }
+    }
+}
