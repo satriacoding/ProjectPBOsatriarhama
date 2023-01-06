910aa9c3298d1351df9ede823057c5b2847fbd69
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:25:56 +0700

    Create lat7.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama271022_modul6/lat7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama271022_modul6;
+
+/**
+ *
+ * @author satri
+ */
+public class lat7 {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("Satria Rhama Dwi Jaya");
+            i++;
+        }while(i<100);
+    }
+}
