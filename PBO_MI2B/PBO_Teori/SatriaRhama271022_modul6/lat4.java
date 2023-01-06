17e2f2c884eb5fe36c21137516bbe1887c59c48e
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:25:30 +0700

    Create lat4.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama271022_modul6/lat4.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama271022_modul6;
+
+/**
+ *
+ * @author satri
+ */
+import java.util.Scanner;
+
+public class lat4 {
+      public static void main(String[] args) {
+        int i, n;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan Angka = ");
+        i = in.nextInt();
+
+        switch(i){
+            case 1 : System.out.println("Angka 1");
+                     break;
+            case 2 : System.out.println("Angka 2");
+                     break;
+            case 3 : System.out.println("Angka 3");
+                     break;
+            case 4 : System.out.println("Angka 4");
+                     break;
+            case 5 : System.out.println("Angka 5");
+                     break;
+            case 6 : System.out.println("Angka 6");
+                     break;
+            case 7 : System.out.println("Angka 7");
+                     break;
+            case 8 : System.out.println("Angka 8");
+                     break;
+            case 9 : System.out.println("Angka 9");
+                     break;
+            case 10 : System.out.println("Angka 10");
+                     break;
+            default : System.out.println("Invalid Number!!");
+        }
+    }
+}
