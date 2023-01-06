10f91cd59979b361da48af31caf3fd796451f797
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:26:20 +0700

    Create lat10.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama271022_modul6/lat10.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama271022_modul6;
+
+/**
+ *
+ * @author satri
+ */
+public class lat10 {
+     public static void main(String[] args) {
+        int p = 3;
+        int hasil = 1;
+
+        int i = 1;
+        while(i<=p){
+            hasil = hasil * 5;
+            System.out.println(5 + "^" + i + "=" + hasil);
+            i++;
+        }
+
+    }
+}
