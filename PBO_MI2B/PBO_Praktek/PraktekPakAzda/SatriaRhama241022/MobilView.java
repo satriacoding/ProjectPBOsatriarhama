70096d8ae6b50f0a1479c31ef3a689f5af5d6953
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 12:35:22 +0700

    Create MobilView.java

---
+++ PBO_MI2B/PBO_Praktek/PraktekPakAzda/SatriaRhama241022/MobilView.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PraktekPakAzda.SatriaRhama241022;
+
+/**
+ *
+ * @author satri
+ */
+public class MobilView {
+    public void printMobilDetails(String mobilName, String mobilKode, String mobilWarna){ 
+	System.out.println("Mobil Detail: "); 
+	System.out.println("Kode  : " + mobilKode); 
+	System.out.println("Name  : " + mobilName);
+        System.out.println("Warna : " + mobilWarna);
+    } 
+}
