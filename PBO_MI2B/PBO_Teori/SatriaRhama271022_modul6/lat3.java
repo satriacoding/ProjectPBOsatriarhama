046bb1489b8dd87d13e568a3b9f86a6da0d70df1
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:25:19 +0700

    Create lat3.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama271022_modul6/lat3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama271022_modul6;
+
+/**
+ *
+ * @author satri
+ */
+import java.util.Scanner;
+
+public class lat3 {
+public static void main(String[] args) {
+        int i, n;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan Angka = ");
+        i = in.nextInt();
+
+        if(i == 1) System.out.println("Angka 1");
+        else if(i == 2) System.out.println("Angka 2");
+        else if(i == 3) System.out.println("Angka 3");
+        else if(i == 4) System.out.println("Angka 4");
+        else if(i == 5) System.out.println("Angka 5");
+        else if(i == 6) System.out.println("Angka 6");
+        else if(i == 7) System.out.println("Angka 7");
+        else if(i == 8) System.out.println("Angka 8");
+        else if(i == 9) System.out.println("Angka 9");
+        else if(i == 10) System.out.println("Angka 10");
+        else System.out.println("Invalid Number!!");
+    }    
+}
