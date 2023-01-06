f757a5651c12e14f92f2b194700251f10c9fe99f
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:34:24 +0700

    Create Lat1.java

---
+++ PBO_MI2B/PBO_Teori/Modul_7/Lat1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_Modul7;
+
+/**
+ *
+ * @author satri
+ */
+public class Lat1 {
+    public static void main(String[] args) {
+        String days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        for (int i=0; i<days.length; i++){
+            System.out.println(days[i]);
+        }
+    }
+}
