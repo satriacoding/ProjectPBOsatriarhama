38210367f924e92469c7c4515e97e1b302996d53
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:04:08 +0700

    Create TheTree.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama_modul3/TheTree.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama_modul3;
+
+/**
+ *
+ * @author satri
+ */
+public class TheTree {
+     public static void main(String[] args) {
+        System.out.println("I think I shall never see, ");
+        System.out.println("a poem as lovely as a tree.");
+        System.out.println("A tree whose hungry muth is pressed");
+        System.out.println("Against the Earth's sweet flowing breast");
+    }
+}
