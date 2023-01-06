1bb40ec2499c90571dc304ad6cbdb078b1e12268
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 12:36:02 +0700

    Create StudentView.java

---
+++ PBO_MI2B/PBO_Praktek/PraktekPakAzda/SatriaRhama241022/StudentView.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PraktekPakAzda.SatriaRhama241022;
+
+/**
+ *
+ * @author satri
+ */
+
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentNim){ 
+	System.out.println("Student Detail: "); 
+	System.out.println("Nim : " + studentNim); 
+	System.out.println("Name : " + studentName); 
+    } 
+}
