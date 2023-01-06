e8b528daac1c52a8f1f17a7fe58e3b03a5372d7c
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:25:39 +0700

    Create lat5.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama271022_modul6/lat5.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama271022_modul6;
+
+/**
+ *
+ * @author satri
+ */
+public class lat5 {
+     public static void main(String[] args) {
+        for(int i=0; i<100; i++){
+            System.out.println("Satria Rhama Dwi Jaya");
+        }
+    }
+}
