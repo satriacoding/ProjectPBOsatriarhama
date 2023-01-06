02d5ffd7d2bf6d77c371b62e3de94482e44bfbcd
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:26:02 +0700

    Create lat8.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama271022_modul6/lat8.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama271022_modul6;
+
+/**
+ *
+ * @author satri
+ */
+public class lat8 {
+     public static void main(String[] args) {
+        int i = 0;
+        while(i<100){
+            System.out.println("Satria Rhama Dwi Jaya");
+            i++;
+        }
+    }
+}
