36308f55aaff6d33b1bf54c89fd2c4b93caa80e8
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 11:40:17 +0700

    Create latihan12.java

---
+++ PBO_MI2B/PBO_Teori/SatriaRhama190922_modul4/latihan12.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanModul5;
+
+/**
+ *
+ * @author hp
+ */
+public class latihan12 {
+    public static void main(String [] args){
+        
+        int x = 10;
+        int y = 23;
+        int z = 5;
+        int a;
+        System.out.println("number 1 = "+x);
+        System.out.println("number 2 = "+y);
+        System.out.println("number 3 = "+z);
+        a = (x>=y)? x:y;
+        a = (z>=y)? z:y;
+        System.out.println("nilai tertingginya adalah angka = "+a);
+    }
+}
