cfe53a2965b96235381180d7177865508e995cde
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 12:35:05 +0700

    Create Mobil.java

---
+++ PBO_MI2B/PBO_Praktek/PraktekPakAzda/SatriaRhama241022/Mobil.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PraktekPakAzda.SatriaRhama241022;
+
+/**
+ *
+ * @author satri
+ */
+public class Mobil {
+    private String kode; 
+    private String name;
+    private String warna;
+
+    public String getKode() { 
+	return kode; 
+    } 
+    public void setKode(String kode) { 
+    	this.kode = kode; 
+    } 
+    public String getName() { 
+    	return name; 
+    } 
+    public void setName(String name) { 
+        this.name = name; 
+    }
+    public String getWarna(){
+        return warna; 
+    }
+    public void setWarna(String warna){
+        this.warna = warna;
+    }
+}
