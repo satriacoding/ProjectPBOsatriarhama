3d8a7005c62f0ba66b2d3ba68942637216cfffa8
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 12:36:20 +0700

    Create Manusia.java

---
+++ PBO_MI2B/PBO_Praktek/PraktekPakAzda/SatriaRhama260922/Manusia.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PraktekPakAzda.SatriaRhama260922;
+
+/**
+ *
+ * @author satri
+ */
+public class Manusia {
+    private int tinggi, berat;
+    private String kulit, nama;  
+    
+    public int get_tinggi(){
+        return tinggi;
+    }
+    
+    public void set_tinggi(int tinggi){
+        this.tinggi = tinggi;
+    }
+    
+    public int get_berat(){
+        return berat;
+    }
+    
+    public void set_berat(int berat){
+        this.berat = berat;
+    }
+    
+    public String get_kulit(){
+        return kulit;
+    }
+    
+    public void set_kulit(String kulit){
+        this.kulit = kulit;
+    }
+    
+    public String get_nama(){
+        return nama;
+    }
+    
+    public void set_nama(String nama){
+        this.nama = nama;
+    }
+}
