42d2383ce50f1d70ad35c5f94b05bb27d0b0558b
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-06 12:35:45 +0700

    Create Student.java

---
+++ PBO_MI2B/PBO_Praktek/PraktekPakAzda/SatriaRhama241022/Student.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PraktekPakAzda.SatriaRhama241022;
+
+/**
+ *
+ * @author satri
+ */
+public class Student {
+    private String nim; 
+	private String name; 
+
+	public String getNim() { 
+		return nim; 
+	} 
+	public void setNim(String nim) { 
+		this.nim = nim; 
+	} 
+	public String getName() { 
+		return name; 
+	} 
+	public void setName(String name) { 
+		this.name = name; 
+	} 
+}
