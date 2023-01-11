fae82f6f940c71c127f974758ebb6dcc3bfeb777
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-12 02:37:32 +0700

    Create Pelanggan.java

---
+++ PBO_MI2B/UASB/model/Pelanggan.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UASB.model;
+
+/**
+ *
+ * @author satri
+ */
+public class Pelanggan {
+    private String kode;
+    private String nama;
+    private int jenispelanggan;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getJenispelanggan() {
+        return jenispelanggan;
+    }
+
+    public void setJenispelanggan(int jenispelanggan) {
+        this.jenispelanggan = jenispelanggan;
+    }
+    
+    
+}
