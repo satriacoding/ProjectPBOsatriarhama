c268aeb630403b1b0dc5d3cc37a23e9130573803
satriacoding <118911569+satriacoding@users.noreply.github.com>
2023-01-14 03:13:19 +0700

    Create ArraySample.java

---
+++ PBO_MI2B/PBO_Praktek/SatriaRhama_08122022/ArraySample.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SatriaRhama_08122022;
+
+/**
+ *
+ * @author satri
+ */
+public class ArraySample {
+   public static void main( String[] args ){
+ int[] ages = new int[100];
+
+ for( int i=0; i<ages.length; i++ ){
+ System.out.print( ages[i] );
+ }
+ } 
+}
